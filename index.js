b24840118cb12c97e448c0f456fb6b237d3806b8
Atsuki Uchida <csatsfapsfu2930@gmail.com>
2022-04-12 13:14:50 -0700

    Added superhero and villains modules with packages

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-const fs = require("fs");
+//superhero module
+const superheroes = require('superheroes');
+ 
+superheroes.all;
+//=> ['3-D Man', 'A-Bomb', …]
+ 
+var mySuperheroName = superheroes.random();
 
-fs.copyFileSync("text1.txt", "text2.txt");
+console.log(mySuperheroName);
+// till here
+
+
+
+// supervillians module from here 
+
+const supervillains = require('supervillains');
+ 
+supervillains.all;
+//=> ['Abattoir', 'Able Crown', …]
+ 
+var supervillainsName = supervillains.random();
+//=> 'Mud Pack'
+
+console.log(supervillainsName);
